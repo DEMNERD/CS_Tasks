59f5136411cc2414efe7f654196c83675f44996c
DEMNERD <demyshka2005@gmail.com>
2021-08-03 16:13:16 +0300

    Done second task (Right triangle)

---
+++ SecondTask/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SecondTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first leg's length: ");
+            double leg1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter second leg's length: ");
+            double leg2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Hypotenuse = {Math.Sqrt(leg1*leg1 + leg2*leg2)}\nS = {leg1*leg2}");
+
+        }
+    }
+}
