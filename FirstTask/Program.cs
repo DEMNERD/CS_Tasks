de86673da659d7b1a38f61190cef26c4d14742f6
DEMNERD <demyshka2005@gmail.com>
2021-08-03 15:56:45 +0300

    Done first task

---
+++ FirstTask/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FirstTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double diameter = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"S = {Math.PI * diameter}\nC = {Math.PI * Math.Pow(diameter / 2, 2)}");
+        }
+    }
+}
