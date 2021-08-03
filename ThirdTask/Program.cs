fac88dcec1e9fbded24f5a051414a5ce02bbb7f6
DEMNERD <demyshka2005@gmail.com>
2021-08-03 22:55:27 +0300

    Done third task (combination of First and Second)

---
+++ ThirdTask/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ThirdTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = "You entered neither \"1\" nor \"2\"";
+            Console.Write("Enter either \"1\" or \"2\" and press Enter: ");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.Write("Enter circle's diameter: ");
+                    double diameter = Convert.ToDouble(Console.ReadLine());
+                    result = $"S = {Math.PI * diameter}\nC = {Math.PI * Math.Pow(diameter / 2, 2)}";
+                    break;
+
+                case "2":
+                    Console.Write("Enter first leg's length: ");
+                    double leg1 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Enter second leg's length: ");
+                    double leg2 = Convert.ToDouble(Console.ReadLine());
+                    result = $"Hypotenuse = {Math.Sqrt(leg1 * leg1 + leg2 * leg2)}\nS = {leg1 * leg2}";
+                    break;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
